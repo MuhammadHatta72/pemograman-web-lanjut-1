02a531175b3f22b7cd27e9399d33010e2d6b7ced
MuhammadHatta72 <attah1335@gmail.com>
2023-02-28 10:58:36 +0700

    Add Migration

--- pertemuan_4/pratikum/database/seeders/PostSeeder.php
+++ pertemuan_4/pratikum/database/seeders/PostSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PostSeeder extends Seeder
 {
@@ -12,6 +13,11 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('posts')->insert([
+            'title' => 'Belajar Model Dengan Laravel',
+            'slug' => ' belajar-model-dengan-laravel',
+            'content' => 'Belajar Laravel itu menyenangkan',
+            'draft' => 0
+        ]);
     }
 }
