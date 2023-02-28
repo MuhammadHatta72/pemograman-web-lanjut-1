abfbb5e8a587fad8c395b9db8955e7cf3678a9aa
MuhammadHatta72 <attah1335@gmail.com>
2023-02-28 10:59:03 +0700

    Add DB Seeder

--- pertemuan_4/pratikum/database/seeders/PostSeeder.php
+++ pertemuan_4/pratikum/database/seeders/PostSeeder.php
@@ -16,7 +16,7 @@ class PostSeeder extends Seeder
         DB::table('posts')->insert([
             'title' => 'Belajar Model Dengan Laravel',
             'slug' => ' belajar-model-dengan-laravel',
-            'content' => 'Belajar Laravel itu menyenangkan',
+            'content' => 'Belajar Laravel itu menyenangkan dan seru',
             'draft' => 0
         ]);
     }
