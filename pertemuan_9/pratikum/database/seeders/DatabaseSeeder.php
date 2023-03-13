d01ce707182ca2c5d0fb20126e0a841d69587f4f
MuhammadHatta72 <attah1335@gmail.com>
2023-03-13 22:10:54 +0700

    feat:pratikum orm with relations - many to many

--- pertemuan_9/pratikum/database/seeders/DatabaseSeeder.php
+++ pertemuan_9/pratikum/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            MahasiswaSeeder::class,
+            // MahasiswaSeeder::class,
+            MataKuliahSeeder::class,
         ]);
     }
 }
